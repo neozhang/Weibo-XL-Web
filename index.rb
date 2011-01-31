f5aa444648ed139beaa3a105e7ad11ceba3454d8
neozhang <neo.zhangning@gmail.com>
2011-01-31 23:51:14 +0800

    first major release

--- index.rb
+++ index.rb
@@ -1,34 +1,68 @@
-require 'sinatra'
-require 'rubygems'
-require 'haml'
-require 'oauth'
-require 'oauth/consumer'
-
-# Create a Weibo oauth instance for authorization process
+%w(sinatra haml oauth sass json timeout weibo).each { |dependency| require dependency }
+enable :sessions
 # http://open.t.sina.com.cn/apps/1002277584
-api_key = '1002277584'
-api_key_secret = '5e7fac721cda71ce62247a6f48316487'
+Weibo::Config.api_key = "2942145647"
+Weibo::Config.api_secret = "5cc0026c470a25a6070237e07ade5f27"
 
 get '/' do
+  puts
+  puts request.env.inspect
+  puts
+  if session[:atoken]
+    redirect "/friends_timeline"
+  else
+    redirect "/connect"
+  end
   haml :index
 end
 
-get '/connect' do
-  # here to allow users to connect to their Sina Weibo accounts
-  @consumer = OAuth::Consumer.new(api_key, api_key_secret,
-                                   {:site => "http://api.t.sina.com.cn",
-                                    :request_token_path => "/oauth/request_token",  
-                                    :access_token_path => "/oauth/access_token",  
-                                    :authorize_path => "/oauth/authorize",  
-                                    :signature_method => "HMAC-SHA1",  
-                                    :scheme => :header,
-                                    :oauth_version => "1.0a"
-                                   }
-                                 )
-  @request_token=@consumer.get_request_token
-  redirect @request_token.authorize_url
-  @access_token=@request_token.get_access_token
+get '/friends_timeline' do
+  @timeline = get_timeline
+  haml :index
+end 
+
+post '/repost/:id' do
+  oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
+  oauth.authorize_from_access(session[:atoken], session[:asecret])
+  Weibo::Base.new(oauth).repost(params[:id])
+  redirect '/'
 end
 
+post '/fav/:id' do
+  oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
+  oauth.authorize_from_access(session[:atoken], session[:asecret])
+  Weibo::Base.new(oauth).favorites_create(params[:id])
+  redirect '/'
+end
 
+get '/connect' do
+  oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
+  request_token = oauth.consumer.get_request_token
+  session[:rtoken], session[:rsecret] = request_token.token, request_token.secret
+  redirect "#{request_token.authorize_url}&oauth_callback=http://#{request.env["HTTP_HOST"]}/callback"
+end
 
+get '/callback' do
+  oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
+  oauth.authorize_from_request(session[:rtoken], session[:rsecret], params[:oauth_verifier])
+  session[:rtoken], session[:rsecret] = nil, nil
+  session[:atoken], session[:asecret] = oauth.access_token.token, oauth.access_token.secret
+  redirect "/"
+end
+
+get '/logout' do
+  session[:atoken], session[:asecret] = nil, nil
+  redirect "/"
+end
+
+get '/screen.css' do
+  content_type 'text/css'
+  sass :screen
+end
+
+def get_timeline()
+  oauth = Weibo::OAuth.new(Weibo::Config.api_key, Weibo::Config.api_secret)
+  oauth.authorize_from_access(session[:atoken], session[:asecret])
+  @timeline = Weibo::Base.new(oauth).friends_timeline()
+  return @timeline
+end
